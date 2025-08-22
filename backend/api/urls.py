2a5c528e8efe6969ee756df7326d904184c94cb7
Aanya Mittal <153476178+aanxieee@users.noreply.github.com>
2025-08-22 16:25:18 +0530

    Delete backend directory

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import StockPredictionView, MarketingPredictionView
-
-urlpatterns = [
-    path('predict/stock/', StockPredictionView.as_view(), name='stock-prediction'),
-    path('predict/marketing/', MarketingPredictionView.as_view(), name='marketing-prediction'),
-]
